3a7a53821ce1bf4f79d5a20e64891e117c655f44
Ashutosh <muleyashutosh@gmail.com>
2021-04-26 00:10:33 +0530

    added error msg to register handler

--- controllers/register.js
+++ controllers/register.js
@@ -1,41 +1,44 @@
 const handleRegister = (req, res, db, bcrypt) => {
-    const {name, email, password} = req.body;
-    if(!email || !password || !name) {
-        return res.status(400).json('incomplete form submission')
-    }
-    const hash = bcrypt.hashSync(password, 10);
-    db.transaction(trx => {
-        db.insert({
-                hash:hash,
-                email: email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                db('users')
-                    .returning('*')
-                    .insert({
-                        name: name,
-                        email: loginEmail[0],
-                        joined: new Date()
-                    })
-                    .then(user => {
-                        // console.log(user)
-                        res.json(user[0])
-                    })
-                    .catch(err => {
-                        console.log(err)
-                        res.status(400).json('Unable to register')
-                    })
-            })
-            .then(trx.commit)
-            .catch(err => {
-                console.log(err)
-                trx.rollback
-                res.status(400).json('Unable to register')
-            })
+  const { name, email, password } = req.body;
+  if (!email || !password || !name) {
+    return res.status(400).json("incomplete form submission");
+  }
+  const hash = bcrypt.hashSync(password, 10);
+  let ErrorMessage = "Unable to register";
+  db.transaction((trx) => {
+    db.insert({
+      hash: hash,
+      email: email,
     })
-    .catch(err => res.status(400).json('Unable to register'))
-}
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        db("users")
+          .returning("*")
+          .insert({
+            name: name,
+            email: loginEmail[0],
+            joined: new Date(),
+          })
+          .then((user) => {
+            // console.log(user)
+            res.json(user[0]);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(400).json(ErrorMessage);
+          });
+      })
+      .then(trx.commit)
+      .catch((err) => {
+        console.log(err);
+        if (err.includes("duplicate key value")) {
+          ErrorMessage = "User Already Exists";
+        }
+        trx.rollback;
+        res.status(400).json(ErrorMessage);
+      });
+  }).catch((err) => res.status(400).json(ErrorMessage));
+};
 
-module.exports = {handleRegister}
+module.exports = { handleRegister };
